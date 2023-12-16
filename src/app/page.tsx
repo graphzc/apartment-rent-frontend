fdc046a4d24ba9b44dbaa826adc00b1dc4e397e6
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-12-16 10:34:56 +0700

    Remove reditect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import { useRouter } from "next/navigation"
-
 export default function MainPage() {
-  const router = useRouter()
-  router.push('/home')
   return (
     <div></div>
   )
