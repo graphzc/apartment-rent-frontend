b2e5f257bc746e6e7e92d4a0da56ad90445ad81a
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2024-02-12 01:08:56 +0700

    Make payment better

---
+++ src/components/PaymentStatusBadge.tsx
@@ -0,0 +1,31 @@
+import { PaymentStatus } from "@/enum/PaymentStatus"
+import { convertPaymentStatusToTh } from "@/utils/paymentStatusUtils";
+
+interface PaymentStatusBadgeProps {
+    status: PaymentStatus;
+}
+
+export default function PaymentStatusBadge({ status } : PaymentStatusBadgeProps) {
+    let color = "";
+    switch (status) {
+        case PaymentStatus.APPROVED:
+            color = "bg-green-500";
+            break;
+        case PaymentStatus.UNPAID:
+            color = "bg-orange-500";
+            break;
+        case PaymentStatus.PENDING:
+            color = "bg-yellow-500";
+            break;
+        case PaymentStatus.REJECTED:
+            color = "bg-red-500";
+            break;
+    }
+
+
+    return (
+        <span className={`px-2 py-1 text-white rounded-md ${color}`}>
+            { convertPaymentStatusToTh(status) }
+        </span>
+    )
+}
---
+++ src/utils/paymentStatusUtils.tsx
@@ -0,0 +1,31 @@
+import { PaymentStatus } from "@/enum/PaymentStatus";
+
+export function convertPaymentStatus(status: PaymentStatus) {
+    switch (status) {
+        case PaymentStatus.UNPAID:
+            return <span className='text-orange-500'>ยังไม่ชำระเงิน</span>;
+        case PaymentStatus.APPROVED:
+            return <span className='text-green-500'>ชำระเงินสำเร็จ</span>;
+        case PaymentStatus.REJECTED:
+            return <span className='text-red-500'>ชำระเงินไม่สำเร็จ</span>;
+        case PaymentStatus.PENDING:
+            return <span className='text-yellow-500'>รอเช็คการชำระเงิน </span>;
+        default:
+            return "Unknown";
+    }
+}
+
+export function convertPaymentStatusToTh(status: PaymentStatus) {
+    switch (status) {
+        case PaymentStatus.UNPAID:
+            return "ยังไม่ชำระเงิน";
+        case PaymentStatus.APPROVED:
+            return "ชำระเงินสำเร็จ";
+        case PaymentStatus.REJECTED:
+            return "ชำระเงินไม่สำเร็จ";
+        case PaymentStatus.PENDING:
+            return "รอเช็คการชำระเงิน";
+        default:
+            return "Unknown";
+    }
+}
