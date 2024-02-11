227b8dcc633b83543ef4d52a178c73ef46205419
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2024-02-12 04:06:38 +0700

    Add apartment and room show

--- src/app/(admin)/admin/room/[id]/page.tsx
+++ src/app/(admin)/admin/room/[id]/page.tsx
@@ -1,25 +1,46 @@
 'use client'
 
+import useRoom from "@/api/room/useRoom";
+import BackButton from "@/components/BackButton";
+import { PaymentStatus } from "@/enum/PaymentStatus";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-import  EditRoomForm from '@/components/admin/room/EditRoomForm';
-import  SubmitButton from '@/components/SubmitButton';
-import Apartment from "@/interface/Apartment";
-import useApartments from "@/api/apartment/useApartments";
-export default function CreateCustomerPage({params} : {params: {id: string}}) {
-    const { isPending: isPendingApartment, error: errorApartment, data: apartments } = useApartments();
-    // if(isPending) return <div>Loading...</div>;
-    // if(error) return <div>{error.message}</div>;
-    const handleUpdate = (data: Apartment) => {
-        // editCustomer.mutate(data);
-    }
 
+export default function ViewRoomPage({params} : {params: {id: string}}) {
+    
+    const router = useRouter();
+
+    const { isPending, error, data: room } = useRoom(parseInt(params.id));
+
+    if (isPending) return <div>Loading...</div>;
+
+    if (error) return <div>{ error.message }</div>;
+
+    const isAvaliable = room.booking.length > 0 && room.booking[0].endDate < new Date() && room.booking[0].payment.length > 0 && room.booking[0].payment[0].status !== PaymentStatus.UNPAID || room.booking.length == 0;
+    
+    console.log(isAvaliable);
     return (
-
         <div>
-             <h1 className="text-xl mb-5">แก้ไขห้องพัก</h1>
-             <div className=" bg-gray-200 py-10 px-5 rounded-xl ">
-                {/* <EditRoomForm apartments={apartments!} /> */}
-             </div>
+            <BackButton href="/admin/room" />
+            <h1 className="text-xl mb-5">ข้อมูลห้องพัก</h1>
+            <div>
+                <div><span className="font-medium">เลขห้อง</span> : { room?.no }</div>
+                <div><span className="font-medium">ราคา</span> : { room?.price }</div>
+                <div><span className="font-medium">อพาร์ทเม้นท์</span> : { room?.apartment.name }</div>
+                <div><span className="font-medium">สถานะ</span> : { isAvaliable ? 'ว่าง' : 'ไม่ว่าง' }</div>
+                { 
+                    !isAvaliable && (
+                        <>
+                            <div><span className="font-medium">ผู้เช่า</span> : { room.booking[0].user.name }</div> 
+                            <div><span className="font-medium">สัญญา</span> : { new Date(room.booking[0].startDate).toLocaleDateString() } - { new Date(room.booking[0].endDate).toLocaleDateString() }</div>
+                        </>
+
+                    )    
+                }
+
+            </div>
+
+
         </div>
     );
 }
