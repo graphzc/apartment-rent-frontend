9e0ce5be23e35ef5979748af5b6f5190e4062a0a
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2025-06-24 01:14:34 +0700

    add env to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Kanit, Mitr, Noto_Sans_Thai, IBM_Plex_Sans_Thai, Prompt, Itim, Krub } from "next/font/google";
+import {
+  Inter,
+  Kanit,
+  Mitr,
+  Noto_Sans_Thai,
+  IBM_Plex_Sans_Thai,
+  Prompt,
+  Itim,
+  Krub,
+} from "next/font/google";
 import Navbar from "./(main)/Navbar";
 import NextAuthProvider from "@/provider/NextAuthProvider";
 import QueryClientProvider from "@/provider/QueryClientProvider";
-import ToastContainer from '@/provider/ToastContainer'
-import 'react-toastify/dist/ReactToastify.css';
+import ToastContainer from "@/provider/ToastContainer";
+import "react-toastify/dist/ReactToastify.css";
+import { PublicEnvScript } from "next-runtime-env";
 
 const prompt = Prompt({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -26,6 +36,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <PublicEnvScript />
+      </head>
       <body className={prompt.className}>
         <NextAuthProvider>
           <QueryClientProvider>
