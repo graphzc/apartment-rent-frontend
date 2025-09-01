a9ecdfb68c12023f4a0f3f33da203623a46c3a7d
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2025-09-01 21:04:45 +0700

    fix build bug

--- src/enum/BookingStatus.ts
+++ src/enum/BookingStatus.ts
@@ -2,5 +2,5 @@ export enum BookingStatus {
     PendingForPayment = "PENDING_FOR_PAYMENT",
     Success = "SUCCESS",
     Cancelled = "CANCELLED",
-    Terminated = "TERMINATED"
+    Terminated = "TERMINATED",
 }
